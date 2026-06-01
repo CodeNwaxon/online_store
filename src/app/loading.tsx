3f4456fdae8194f374db465d0a94b065c8e5ccf4
nwaxon2020 <princenwachukwu308@yahoo.com>
2026-06-01 12:44:32 +0100

    loading adjusted

--- src/app/loading.tsx
+++ src/app/loading.tsx
@@ -32,12 +32,12 @@ export default function Loading() {
         <div className="absolute inset-0 rounded-full border-4 border-muted border-t-primary animate-spin" />
 
         {/* Small Logo in Center */}
-        <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 w-[50px] h-[50px] overflow-hidden p-1 bg-white">
+        <div className="rounded-md absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 w-[55px] h-[55px] overflow-hidden p-1 bg-white">
           <Image
             src="/nomo_lg.png"
             alt="Loading..."
-            width={42}
-            height={42}
+            width={47}
+            height={47}
             className="object-contain"
           />
         </div>
